e2d4198ab868f18189dc8c3d2c824fe7fd2b7e73
aurora-721 <aurora.makovac@gmail.com>
2022-08-22 07:16:36 +0200

    adding contract

--- pages/api/store-event-data.js
+++ pages/api/store-event-data.js
@@ -12,7 +12,27 @@ async function storeEventData(req, res) {
         .status(500)
         .json({ error: "Error creating event", success: false });
     }
-  }
+}
+
+async function makeFileObjects(body) {
+  const buffer = Buffer.from(JSON.stringify(body));
+
+  const imageDirectory = resolve(process.cwd(), `public/images/${body.image}`);
+  const files = await getFilesFromPath(imageDirectory);
+
+  files.push(new File([buffer], "data.json"));
+  return files;
+}
+
+function makeStorageClient() {
+  return new Web3Storage({ token: process.env.WEB3STORAGE_TOKEN });
+}
+
+async function storeFiles(files) {
+  const client = makeStorageClient();
+  const cid = await client.put(files);
+  return cid;
+}
 
 export default async function handler(req, res) {
     if (req.method === "POST") {
